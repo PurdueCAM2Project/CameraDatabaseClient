0f0f1aaa57167618979601aca5de83d1bde84ed6
sripathm22 <mishra60@purdue.edu>
2018-04-06 16:28:41 -0400

    added code to get the token. not working

--- pythonAPIClient/client.py
+++ pythonAPIClient/client.py
@@ -1,5 +1,5 @@
-from .camera import Camera, IPCamera, NonIPCamera, StreamCamera
-from .error import Error
+#from .camera import Camera, IPCamera, NonIPCamera, StreamCamera
+#from .error import Error
 import requests
 
 class Client(object):
@@ -9,7 +9,11 @@ class Client(object):
     # TODO: corresponding to the auth route
     @staticmethod
     def request_token(id, secret):
-        return 'dummy'
+        url = 'https://cam2-api.herokuapp.com/auth/'
+        data = {'clientID': id, 'clientSecret': secret}
+        response = requests.post(url, json=data)
+        print(response)
+        return response.json()['token']
 
     # TODO: set authentication in header
     @staticmethod
@@ -71,3 +75,8 @@ class Client(object):
     # replace others with desired field names
     def search_camera(self, others):
         pass
+
+if __name__ == '__main__':
+    clinetID = "dd53cbd9c5306b1baa103335c4b3e91d8b73386ba29124ea2b1d47a619c8c066877843cd8a7745ce31021a8d1548cf2a"
+    clientSecret = "f7ad9184949b914f2c73da4f6c82f6e93660c23c644e0ca4c9ac8268a141e02f258728aeefe864a6d03f709163cefe9c"
+    km = Client(clinetID,clientSecret);
