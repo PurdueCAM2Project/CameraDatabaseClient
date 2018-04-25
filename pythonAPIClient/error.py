887ea02bf0508b2a615d8ccb5a06c0df19819c37
LING ZHANG <junelena1313@gmail.com>
2018-04-25 19:02:44 -0400

    error handling format

--- pythonAPIClient/error.py
+++ pythonAPIClient/error.py
@@ -9,6 +9,13 @@ class FormatError(Error):
     """
     Corresponends to 422 RequestFormatError in API.
     """
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return str(self.message)
+
     pass
 
 
@@ -44,15 +51,29 @@ class InternalError(Error):
     """
     Corresponends to 500 InternalError in API.
     """
+
+    def __init__(self):
+        self.message = 'InternalError'
+
+    def __str__(self):
+        return str(self.message)
+
     pass
 
 
 class ResourceConflictError(Error):
     """
     Corresponends to 409 ResourceConflictError in API.
-    It is only used in /cams/create route
-    signaling 'cameraIDAlreadyExist'
+    It is used in POST /cameras/create and PUT /cameras/{:camerID} route
+    signaling 'legacyCameraIDAlreadyExist'
     """
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return str(self.message)
+
     pass
 
 
@@ -60,6 +81,13 @@ class AuthorizationError(Error):
     """
     Corresponends to 403 AuthorizationError in API.
     """
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return str(self.message)
+
     pass
 
 
