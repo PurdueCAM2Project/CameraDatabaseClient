ec10cd84cb9751992dd79916c3b665184f18126d
Sripath Mishra <mishra60@purdue.edu>
2018-06-14 14:14:45 -0400

    2.1 to fix

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -15,22 +15,28 @@ class TestCamera(unittest.TestCase):
                      1, 0, 0, "", "",
                      "", "null", "test_url")
 
-        ip_cam_test1 = IPCamera(1, "ip", "test_source", 10, 50, "test_country", "test_state", "test_city",
-                                1, 1, 0, 0, "null", "null", "null", "null", "test_url", "ip", "test_ip_port",
-                                "test_brand", "test_model", "test_image_path", "test_video_path")
-        ip_cam_test2 = IPCamera(1, "ip", "test_source", 10, 50, "test_country", "test_state", "test_city",
-                                1, 1, 0, 0, "null", "null", "null", "null", "test_url", "ip", "test_ip_port",
-                                "test_brand", "test_model", "test_image_path", None)
+        ip_cam_test1 = IPCamera(1, "ip", "test_source", 10, 50, "test_country", "test_state",
+                                "test_city", 1, 1, 0, 0, "null", "null", "null", "null",
+                                "test_url", "ip", "test_ip_port", "test_brand", "test_model",
+                                "test_image_path", "test_video_path")
+        ip_cam_test2 = IPCamera(1, "ip", "test_source", 10, 50, "test_country", "test_state",
+                                "test_city", 1, 1, 0, 0, "null", "null", "null", "null",
+                                "test_url", "ip", "test_ip_port", "test_brand", "test_model",
+                                "test_image_path", None)
 
-        non_ip_cam_test1 = NonIPCamera(1, "nonip", "test_source", 10, 50, "test_country", "test_state", "test_city",
-                                       1, 1, 0, 0, "null", "null", "null", "null", "test_url", "test_snapshot_url")
-        non_ip_cam_test2 = NonIPCamera(1, "nonip", "test_source", 10, 50, "test_country", "test_state", "test_city",
-                                       1, 1, 0, 0, "null", "null", "null", "null", "test_url", None)
+        non_ip_cam_test1 = NonIPCamera(1, "nonip", "test_source", 10, 50, "test_country",
+                                       "test_state", "test_city", 1, 1, 0, 0, "null",
+                                       "null", "null", "null", "test_url", "test_snapshot_url")
+        non_ip_cam_test2 = NonIPCamera(1, "nonip", "test_source", 10, 50, "test_country",
+                                       "test_state", "test_city", 1, 1, 0, 0, "null", "null",
+                                       "null", "null", "test_url", None)
 
-        stream_cam_test1 = StreamCamera(1, "stream", "test_source", 10, 50, "test_country", "test_state", "test_city",
-                                        1, 1, 0, 0, "null", "null", "null", "null", "test_url", "test_m3u8_url")
-        stream_cam_test2 = StreamCamera(1, "stream", "test_source", 10, 50, "test_country", "test_state", "test_city",
-                                        1, 1, 0, 0, "null", "null", "null", "null", "test_url", None)
+        stream_cam_test1 = StreamCamera(1, "stream", "test_source", 10, 50, "test_country",
+                                        "test_state", "test_city", 1, 1, 0, 0, "null", "null",
+                                        "null", "null", "test_url", "test_m3u8_url")
+        stream_cam_test2 = StreamCamera(1, "stream", "test_source", 10, 50, "test_country",
+                                        "test_state", "test_city", 1, 1, 0, 0, "null", "null",
+                                        "null", "null", "test_url", None)
 
         self.assertTrue(isinstance(cam, Camera))
 
