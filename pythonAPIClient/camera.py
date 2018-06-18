7fca32ed6f9e32e30648ad4e81797dede2129047
mush-zhang <junelena1313@gmail.com>
2018-06-18 15:26:12 -0400

    rewrite camera constructor

--- pythonAPIClient/camera.py
+++ pythonAPIClient/camera.py
@@ -1,88 +1,46 @@
 """
-This class holds the code to make camera objects.
+Represents a camera.
 """
-
 
 class Camera(object):
     """
-    Represent a single camera
+    Represent a general camera.
+    This class includes fields common for all types of camera.
     """
-    def __init__(self, cameraID, camera_type, source, lat, lng, country, state, city,
-                 resolution_width, resolution_height, is_active_image,
-                 is_active_video, utc_offset, timezone_id,
-                 timezone_name, reference_logo, reference_url):
-        self.cameraID = cameraID
-        self.camera_type = camera_type
-        self.source = source
-        self.lat = lat
-        self.lng = lng
-        self.country = country
-        self.state = state
-        self.city = city
-        self.resolution_width = resolution_width
-        self.resolution_height = resolution_height
-        self.is_active_image = is_active_image
-        self.is_active_video = is_active_video
-        self.utc_offset = utc_offset
-        self.timezone_id = timezone_id
-        self.timezone_name = timezone_name
-        self.reference_logo = reference_logo
-        self.reference_url = reference_url
 
+    def __init__(self, **dict_entries):
+        self.__dict__.update(dict_entries)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    @staticmethod
+    def process_json(**dict_entries):
+        dict_entries['camera_type'] = dict_entries['type']
+        dict_entries.pop('type', None)
+        dict_entries.update(dict_entries['retrieval'])
+        dict_entries.pop('retrieval', None)
+
+        if dict_entries['camera_type'] == 'ip':
+            return IPCamera(**dict_entries)
+        if dict_entries['camera_type'] == 'non_ip':
+            return NonIPCamera(**dict_entries)
+        return StreamCamera(**dict_entries)
 
 class IPCamera(Camera):
     """
-    Represent a single ip_camera
-    This is a subclass of Camera
+    Represent a single ip_camera.
+    This is a subclass of Camera.
     """
-    def __init__(self, cameraID, camera_type, source, lat, lng, country, state,
-                 city, resolution_width, resolution_height, is_active_image,
-                 is_active_video, utc_offset, timezone_id, timezone_name,
-                 reference_logo, reference_url, ip, port, brand, model, image_path, video_path):
-        self.ip = ip
-        self.port = port
-        self.brand = brand
-        self.model = model
-        self.image_path = image_path
-        self.video_path = video_path
-        super(IPCamera, self).__init__(cameraID, camera_type, source, lat, lng,
-                                       country, state, city, resolution_width,
-                                       resolution_height, is_active_image, is_active_video,
-                                       utc_offset, timezone_id, timezone_name,
-                                       reference_logo, reference_url)
-
 
 class NonIPCamera(Camera):
     """
     Represent a non-IP camera.
-    This is a subclass of Camera
+    This is a subclass of Camera.
     """
-    def __init__(self, cameraID, camera_type, source, lat, lng, country, state, city,
-                 resolution_width, resolution_height, is_active_image, is_active_video,
-                 utc_offset, timezone_id, timezone_name, reference_logo,
-                 reference_url, snapshot_url):
-        self.snapshot_url = snapshot_url
-        super(NonIPCamera, self).__init__(cameraID, camera_type, source, lat, lng,
-                                          country, state, city, resolution_width,
-                                          resolution_height, is_active_image,
-                                          is_active_video, utc_offset,
-                                          timezone_id, timezone_name,
-                                          reference_logo, reference_url)
-
 
 class StreamCamera(Camera):
     """
     Represent a Stream camera.
     This is a subclass of Camera
     """
-    def __init__(self, cameraID, camera_type, source, lat, lng, country, state, city,
-                 resolution_width, resolution_height, is_active_image, is_active_video,
-                 utc_offset, timezone_id, timezone_name, reference_logo, reference_url,
-                 m3u8_url):
-        self.m3u8_url = m3u8_url
-        super(StreamCamera, self).__init__(cameraID, camera_type, source, lat, lng,
-                                           country, state, city, resolution_width,
-                                           resolution_height, is_active_image,
-                                           is_active_video, utc_offset,
-                                           timezone_id, timezone_name,
-                                           reference_logo, reference_url)
