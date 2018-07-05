bd8ffb33da2422762a98a3a3aeb61e3db6c74825
mush-zhang <junelena1313@gmail.com>
2018-07-05 13:38:06 -0400

    method cam_match check if given type and retrieval of a camera, is there a matching camera in the db that has the same retrieval

--- pythonAPIClient/client.py
+++ pythonAPIClient/client.py
@@ -391,3 +391,68 @@ class Client(object):
         for current_object in camera_response_array:
             camera_processed.append(Camera.process_json(**current_object))
         return camera_processed
+
+    def cam_match(self, camera_type, **kwargs):
+        """
+        A method to get one or more camera object that has the given retrieval method
+        in the database.
+
+        Parameters
+        ----------
+        camera_type : str
+            Type of the camera. Type can only be 'ip', 'non_ip', or 'stream'.
+        ip : str, optional
+            [for IP camera] Ip address of the camera. Although marked as optional, this field is required
+            when the camera type is 'ip'.
+        port : Int, optional
+            [for IP camera] Port of the camera. If no port provided, it will be set to default 80.
+        image_path : str, optional
+            [for IP camera] Path to retrieve images from the camera.
+        video_path : str, optinal
+            [for IP camera] Path to retrievae vidoe from the camera.
+        snapshot_url : str, optional
+            [for non_IP camera] Url to retrieval image frames from the camera. Although marked as optional, this field is required
+            when the camera type is 'non_ip'.
+        m3u8_url : str, optional
+            [for stream camera] Url to retrieval video stream from the camera. Although marked as optional, this field is required
+            when the camera type is 'stream'.
+
+        Returns
+        -------
+        :list
+            List of camera objects that has the given retrieval method. If there ar eno cameras
+            matches the provided retrieval information, an empty list will be returned.
+
+        """
+        # function locals() contain all local variables;
+        # use another variable param to build a dict in the first line of the method
+        # will catch all method parameters & their values without catching other local variables
+        # used in the method but not used in api call.
+
+        param = dict(locals())
+        param.pop('self')
+        url = Client.base_URL + "apps/match"
+
+        # validate parameter names here.
+
+        if self.token is None:
+            self.request_token()
+        header = self.header_builder()
+        response = self._check_token(response=requests.get(url, headers=header, params=param),
+                                     flag='GET', url=url, params=param)
+        if response.status_code != 200:
+            if response.status_code == 401:
+                raise AuthenticationError(response.json()['message'])
+            elif response.status_code == 403:
+                raise AuthorizationError(response.json()['message'])
+            elif response.status_code == 404:
+                raise ResourceNotFoundError(response.json()['message'])
+            elif response.status_code == 422:
+                raise FormatError(response.json()['message'])
+            else:
+                raise InternalError()
+        camera_response_array = response.json()
+        camera_processed = []
+        for current_object in camera_response_array:
+            camera_processed.append(Camera.process_json(**current_object))
+        return camera_processed
