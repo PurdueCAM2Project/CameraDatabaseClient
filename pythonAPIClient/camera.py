30006be05f675d3c5f1f6d7368c9caf07efe3595
Mohamad Alani <m98.alani@gmail.com>
2018-07-06 10:08:37 -0400

    Fixed camera import typo

--- pythonAPIClient/camera.py
+++ pythonAPIClient/camera.py
@@ -5,8 +5,6 @@ Represents a camera.
 class Camera(object):
 
     """Class representing a general camera.
-
-
     Attributes
     ----------
     cameraID : str
@@ -30,26 +28,19 @@ class Camera(object):
     timezone_name : str, optional
     reference_logo : str, optional
     reference_url : str, optional
-
     """
 
     def __init__(self, **dict_entries):
         """Client initialization method.
-
         Parameters
         ----------
         dict_entries: dict
             Dictionary of all field values of a camera.
-
         Note
         ----
-
             User should not construct any camera object on his/her own.
-
             Camera should only be initialized by results returned from the API.
-
             Documentation of camera constructor is for CAM2 API team only.
-
         """
         self.__dict__.update(dict_entries)
 
@@ -70,77 +61,30 @@ class Camera(object):
         return StreamCamera(**dict_entries)
 
 class IPCamera(Camera):
+    """Represent a single ip camera.
+    This is a subclass of Camera.
+    Attributes
+    ----------
+    ip : str
+    port : str
+    brand : str, optional
+    model : str, optional
+    image_path : str, optional
+    video_path : str, optional
     """
-    Represent a single ip_camera
-    This is a subclass of Camera
+
+class NonIPCamera(Camera):
+    """Represent a single non-ip camera.
+    This is a subclass of Camera.
+    Attributes
+    ----------
+    snapshot_url : str
     """
 
-    def __init__(self, cameraID, camera_type, source, lat, lng, country, state,
-                 city, resolution_width, resolution_height, is_active_image,
-                 is_active_video, utc_offset, timezone_id, timezone_name,
-                 reference_logo, reference_url, ip, port, brand, model, image_path, video_path):
-        self.ip = ip
-        self.port = port
-        self.brand = brand
-        self.model = model
-        self.image_path = image_path
-        self.video_path = video_path
-        super(IPCamera, self).__init__(cameraID, camera_type, source, lat, lng,
-                                       country, state, city, resolution_width,
-                                       resolution_height, is_active_image, is_active_video,
-                                       utc_offset, timezone_id, timezone_name,
-                                       reference_logo, reference_url)
-
-    # TODO: define extra retrieval attributes and constructor of ip camera object
-    # replace others with desired field names
-    def __init__(self,cameraID, camera_type, source, lat, lng, country, state, city, resolution_width
-                , resolution_height, is_active_image, is_active_video, utc_offset, timezone_id
-                , timezone_name, reference_logo, reference_url, ip,port,brand,model,image_path,video_path):
-                self.ip = ip
-                self.port = port
-                self.brand = brand
-                self.model = model
-                self.image_path = image_path
-                self.video_path = video_path
-                super(IPCamera, self).__init__(cameraID, camera_type, source, lat, lng, country, state, city, resolution_width
-                , resolution_height, is_active_image, is_active_video, utc_offset, timezone_id
-                , timezone_name, reference_logo, reference_url)
-    def asdict(self):
-        data = super().asdict()
-        data['retrieval'] = {
-            "ip": self.ip,
-            "port": self.port,
-            "brand": self.brand,
-            "model": self.model,
-            "image_path": self.image_path,
-            "video_path": self.video_path
-        }
-
-    class IPCamera(Camera):
-        """Represent a single ip camera.
-        This is a subclass of Camera.
-        Attributes
-        ----------
-        ip : str
-        port : str
-        brand : str, optional
-        model : str, optional
-        image_path : str, optional
-        video_path : str, optional
-        """
-
-    class NonIPCamera(Camera):
-        """Represent a single non-ip camera.
-        This is a subclass of Camera.
-        Attributes
-        ----------
-        snapshot_url : str
-        """
-
-    class StreamCamera(Camera):
-        """Represent a single stream camera.
-        This is a subclass of Camera.
-        Attributes
-        ----------
-        m3u8_url : str
-        """
+class StreamCamera(Camera):
+    """Represent a single stream camera.
+    This is a subclass of Camera.
+    Attributes
+    ----------
+    m3u8_url : str
+    """
