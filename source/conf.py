268f2c6800586918714562ca1077eb119b51e425
mrjijmjr <dailey1@purdue.edu>
2018-10-01 19:57:44 -0400

    added simple tutorial

--- source/conf.py
+++ source/conf.py
@@ -14,13 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
-
-from sphinx.ext.autosummary import Autosummary
-from sphinx.ext.autosummary import get_documenter
-from docutils.parsers.rst import directives
-from sphinx.util.inspect import safe_getattr
-import re
+# sys.path.insert(0, os.path.abspath('../..'))
+sys.path.append(os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -53,6 +48,7 @@ extensions = [
     'sphinx.ext.autosummary',
 ]
 
+autosummary_generate = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -170,47 +166,3 @@ texinfo_documents = [
 # -- Extension configuration -------------------------------------------------
 autodoc_member_order = 'bysource'
 autoclass_content = 'both'
-
-
-class AutoAutoSummary(Autosummary):
-
-    option_spec = {
-        'methods': directives.unchanged,
-        'attributes': directives.unchanged
-    }
-
-    required_arguments = 1
-
-    @staticmethod
-    def get_members(obj, typ, include_public=None):
-        if not include_public:
-            include_public = []
-        items = []
-        for name in dir(obj):
-            try:
-                documenter = get_documenter(safe_getattr(obj, name), obj)
-            except AttributeError:
-                continue
-            if documenter.objtype == typ:
-                items.append(name)
-        public = [x for x in items if x in include_public or not x.startswith('_')]
-        return public, items
-
-    def run(self):
-        clazz = str(self.arguments[0])
-        try:
-            (module_name, class_name) = clazz.rsplit('.', 1)
-            m = __import__(module_name, globals(), locals(), [class_name])
-            c = getattr(m, class_name)
-            if 'methods' in self.options:
-                _, methods = self.get_members(c, 'method', ['__init__'])
-
-                self.content = ["~%s.%s" % (clazz, method) for method in methods if not method.startswith('_')]
-            if 'attributes' in self.options:
-                _, attribs = self.get_members(c, 'attribute')
-                self.content = ["~%s.%s" % (clazz, attrib) for attrib in attribs if not attrib.startswith('_')]
-        finally:
-            return super(AutoAutoSummary, self).run()
-
-def setup(app):
-    app.add_directive('autoautosummary', AutoAutoSummary)
